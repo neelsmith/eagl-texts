50999ca7ffe07d5da50fdfa78ca7c4b82164ba64
Neel Smith <dnsmith.neel@gmail.com>
2023-01-10 07:08:00 -0500

    Update Lysias reader

--- scripts/syntaxsite.jl
+++ scripts/syntaxsite.jl
@@ -181,10 +181,10 @@ function publishall(sentences, groups, tokens)
     @info("Done: wrote $(length(sentences)) HTML pages linked to accompanying PNG file in $(outputdir). (Now in $(pwd()))")
 end
 
-#publishall(sentences, groups, tokens)
+publishall(sentences, groups, tokens)
 
 
 # This works if you want to republish a specific sentence identified
 # by its sequence number:
-publishsentence(14, sentences, groups, tokens)  
+#publishsentence(16, sentences, groups, tokens)  
 
