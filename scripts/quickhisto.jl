ced77928d0216120a9ba720e2f9f11c3c23494c4
Neel Smith <dnsmith.neel@gmail.com>
2023-05-20 16:30:46 -0400

    Break out histos

--- scripts/quickhisto.jl
+++ scripts/quickhisto.jl
@@ -6,9 +6,10 @@ xenfile = joinpath(pwd(), "texts", "oeconomicus.cex")
 lysfile = joinpath(pwd(), "texts", "lysias1.cex")
 
 xencorp = fromcex(xenfile, CitableTextCorpus, FileReader)
-lyscorp = fromcex(lysfile, CitableTextCorpus, FileReader)
+xenhist = corpus_histo(xencorp, literaryGreek(), filterby = LexicalToken())
 
+lyscorp = fromcex(lysfile, CitableTextCorpus, FileReader)
+lyshist = corpus_histo(lyscorp, literaryGreek(), filterby = LexicalToken())
 
 c = vcat(lyscorp.passages, xencorp.passages) |> CitableTextCorpus
-
 hist = corpus_histo(c, literaryGreek(), filterby = LexicalToken())
