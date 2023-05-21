bcd19d09aedd27598c11bde749f41404407784a7
Neel Smith <dnsmith.neel@gmail.com>
2023-05-21 13:10:02 -0400

    Add quick histo

--- scripts/quickhisto.jl
+++ scripts/quickhisto.jl
@@ -1,15 +1,15 @@
 using CitableBase, CitableText, CitableCorpus
 using Orthography, PolytonicGreek
-
+using Kanones
 
 xenfile = joinpath(pwd(), "texts", "oeconomicus.cex")
 lysfile = joinpath(pwd(), "texts", "lysias1.cex")
 
 xencorp = fromcex(xenfile, CitableTextCorpus, FileReader)
-xenhist = corpus_histo(xencorp, literaryGreek(), filterby = LexicalToken())
+xenhist = corpus_histo(xencorp, literaryGreek(), filterby = LexicalToken(), normalizer = knormal)
 
 lyscorp = fromcex(lysfile, CitableTextCorpus, FileReader)
-lyshist = corpus_histo(lyscorp, literaryGreek(), filterby = LexicalToken())
+lyshist = corpus_histo(lyscorp, literaryGreek(), filterby = LexicalToken(), normalizer = knormal)
 
 c = vcat(lyscorp.passages, xencorp.passages) |> CitableTextCorpus
-hist = corpus_histo(c, literaryGreek(), filterby = LexicalToken())
+hist = corpus_histo(c, literaryGreek(), filterby = LexicalToken(), normalizer = knormal)
